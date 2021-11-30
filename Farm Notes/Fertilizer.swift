aba12e8dd1ca0718773f47bc332d984a9d5dd23c
alberto.giambone@gmail.com <alberto.giambone@gmail.com>
2021-11-30 21:44:13 +0100

    Fertilizer Model Created

---
+++ Farm Notes/Fertilizer.swift
@@ -0,0 +1,33 @@
+//
+//  Fertilizer.swift
+//  Farm Notes
+//
+//  Created by Alberto Giambone on 30/11/21.
+//
+
+import Foundation
+
+
+struct Fertilization {
+    
+    var N: String
+    var P: String
+    var K: String
+    var kg: String
+    var fertDate: Date
+    
+    var dict: [String: Any] {
+        return[
+        
+            "N": N,
+            "P": P,
+            "K": K,
+            "kg": kg,
+            "fertDate": fertDate
+        
+        ]
+        
+    }
+    
+    
+}
